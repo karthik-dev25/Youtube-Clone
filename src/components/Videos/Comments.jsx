ef02b69e538e781882704794fa4903d46f6af635
Karthik <karthkn25.dev@gmail.com>
2025-05-17 18:36:35 +0530

    Components Removed

--- src/components/Videos/Comments.jsx
+++
@@ -1,9 +0,0 @@
-const Comments = () => {
-  return (
-    <div>
-      <Video />
-    </div>
-  );
-};
-
-export default Comments;
--- src/components/Videos/Video.jsx
+++
@@ -1,5 +0,0 @@
-const Video = () =>{
-    return <div>Video Component</div>
-};
-
-export default Video;
